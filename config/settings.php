7d77f15b3ade1f7d20f1207a7915eea2dcba175b
Tommy Maulana <tommmy.maulana@gmail.com>
2021-03-16 03:57:12 +0100

    first commit

---
+++ config/settings.php
@@ -0,0 +1,9 @@
+<?php
+define('URL', 'https://id.roempi.co/');
+define('DB_HOST', 'localhost');
+define('DB_USER', 'idroempiusr');
+define('DB_PASSWORD', 'idroempi2021');
+define('DB_NAME', 'idroempidb');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'cn_');
+?>
